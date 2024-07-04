cc3b29a69e7a19c28137afdb91f466a1fcfa06bc
hoang19020302 <tranichhoang2001@gmail.com>
2024-07-04 22:39:15 +0700

    config start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import { Howl } from 'howler';
 import './App.css';
+//import soundURL from './assets/hey_sondn.mp3';
+
+// var sound = new Howl({
+//   src: [soundURL],
+// });
+
+// sound.play();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const video = useRef();
+    const init = async () => {
+        console.log('init...');
+        await setupCamera();
+        console.log('setup camera success');
+    };
+
+    const setupCamera = () => {
+        return new Promise((resolve, reject) => {
+            navigator.getUserMedia =
+                navigator.getUserMedia ||
+                navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia ||
+                navigator.msGetUserMedia;
+
+            if (navigator.getUserMedia) {
+                navigator.getUserMedia(
+                    {
+                        video: true,
+                    },
+                    (stream) => {
+                        video.current.srcObject = stream;
+                        video.current.addEventListener('loadeddata', resolve);
+                    },
+                    (error) => {
+                        reject(error);
+                    },
+                );
+            } else {
+                reject();
+            }
+        });
+    };
+
+    useEffect(() => {
+        init();
+
+        // Clean up
+        return () => {};
+    }, []);
+
+    return (
+        <div className="main">
+            <video className="video" autoPlay ref={video} />
+
+            <div className="control">
+                <button className="btn">Train 1</button>
+                <button className="btn">Train 2</button>
+                <button className="btn">Run</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
